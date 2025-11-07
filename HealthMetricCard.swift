4afe204e165b8be7127142acaf30191ca84a53dd
Andrew Tucker <andrew.tuckerrr23@gmail.com>
2025-11-06 22:06:03 -0500

    HealthKit integration + new Info.plist + dashboard card

---
+++ HealthMetricCard.swift
@@ -0,0 +1,18 @@
+//
+//  HealthMetricCard.swift
+//  GaitCoach
+//
+//  Created by Andrew Tucker on 2025-11-06.
+//
+
+import SwiftUI
+
+struct HealthMetricCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HealthMetricCard()
+}
